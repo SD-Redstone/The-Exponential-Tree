738bb9ff1b3604288fa99a7bcb065a1c4d1b8c9e
SD-Redstone <owershek2012@gmail.com>
2024-06-22 11:28:03 +0800

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name: "The Exponential Tree",
+	id: "exptr",
+	author: "SD-Redstone",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
