bfbe953de00b5243d5f54c783ce1b4ed6df22ab3
SD-Redstone <owershek2012@gmail.com>
2024-06-22 12:48:27 +0800

    finished layer 1
    eeeeeeeee

--- js/layers.js
+++ js/layers.js
@@ -6,15 +6,16 @@ addLayer("p", {
         unlocked: true,
 		points: new Decimal(0),
     }},
-    color: "#4BDC13",
+    color: "#0ff",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
     resource: "prestige points", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
-    gainMult() { // Calculate the multiplier for main currency from bonuses
-        mult = new Decimal(1)
+    gainMult() {
+        let mult = new Decimal(1)
+        if (hasUpgrade('p', 13)) mult = mult.times(upgradeEffect('p', 13))
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
@@ -31,5 +32,23 @@ addLayer("p", {
             description: "Multiplies your point gain by 2.",
             cost: new Decimal(1),
         },
+        12: {
+            title: "Mult 2",
+            description: "Multiplies your point gain according to your pestige points.",
+            cost: new Decimal(3),
+            effect() {
+                return player[this.layer].points.add(1).pow(0.5)
+            },
+            effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect
+        
+        },
+        13: {
+            title: "Reverse Mult 2",
+            description: "Multiplies your prestige point gain according to your points.",
+            cost: new Decimal(6),
+            effect() {
+                return player.points.add(1).pow(0.15)
+            },
+        }
     },
 })
--- js/mod.js
+++ js/mod.js
@@ -13,8 +13,8 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.0",
-	name: "Literally nothing",
+	num: "0.1",
+	name: "Test 1",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
@@ -41,11 +41,10 @@ function canGenPoints(){
 function getPointGen() {
 	if(!canGenPoints())
 		return new Decimal(0)
-
-	let gain = new Decimal(1)
-	cost: new Decimal(10) 
-	if (hasUpgrade('p', 11)) gain = gain.times(2)
-	return gain
+let gain = new Decimal(1)
+if (hasUpgrade('p', 11)) gain = gain.times(2)
+		if (hasUpgrade('p', 12)) gain = gain.times(upgradeEffect('p', 12))
+  return gain
 }
 
 // You can add non-layer related variables that should to into "player" and be saved here, along with default values
